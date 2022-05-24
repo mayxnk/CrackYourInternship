ca38bc171ec42d8c29c75ea13d68e412a2978554
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-05-25 00:12:40 +0530

    Time: 18 ms (46.23%), Space: 18.4 MB (75.01%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
     int removeDuplicates(vector<int>& nums)
     {
         //Brute Force Approach : Time - O(nlogn + n), Space - O(1)
-        unordered_set<int> freq;
+        /*unordered_set<int> freq;
         for(int i = 0;i<nums.size();i++)
         {
             freq.insert(nums[i]);
@@ -18,5 +18,17 @@ public:
         }
         sort(nums.begin(),nums.begin() + freq.size());
         return freq.size();
+        */
+        //Optimal Approach :Time - O(n)
+        int i = 0,j = 1;
+        for(;j<nums.size();j++)
+        {
+            if(nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
 ;    }
 };
