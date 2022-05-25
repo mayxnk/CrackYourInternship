60da78f03ee0f57ed9996e872ff2f55f985b7867
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-05-25 18:22:19 +0530

    Time: 26 ms (21.76%), Space: 13.1 MB (95.86%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -22,22 +22,42 @@ public:
     }
     void setZeroes(vector<vector<int>>& matrix) 
     {
-        //Using space O(m*n)
-        vector<vector<bool>> check(matrix.size(), vector<bool> (matrix[0].size()));
-        for(int i = 0;i<matrix.size();i++)
+        //Using space O(m*n) , Time - O(n^3) in worst case
+        // vector<vector<bool>> check(matrix.size(), vector<bool> (matrix[0].size()));
+        // for(int i = 0;i<matrix.size();i++)
+        // {
+        //     for(int j = 0;j<matrix[0].size();j++)
+        //         check[i][j] = false;
+        // }
+        // for(int i = 0;i<matrix.size();i++)
+        // {
+        //     for(int j = 0;j<matrix[0].size();j++)
+        //     {
+        //         if(!matrix[i][j] && !check[i][j])
+        //         {
+        //             makeZero(matrix,check,i,j);
+        //         }
+        //     }
+        // }
+        /*Optimsed */
+        int m = matrix.size(),n = matrix[0].size();
+        int col = 1;
+        for(int i = 0;i<m;i++)
         {
-            for(int j = 0;j<matrix[0].size();j++)
-                check[i][j] = false;
+            if(matrix[i][0] == 0)
+                col = 0;
+            for(int j = 1;j<n;j++)
+                if(matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
         }
-        for(int i = 0;i<matrix.size();i++)
+        for(int i = m-1;i>=0;i--)
         {
-            for(int j = 0;j<matrix[0].size();j++)
-            {
-                if(!matrix[i][j] && !check[i][j])
-                {
-                    makeZero(matrix,check,i,j);
-                }
-            }
+            for(int j = n-1;j>=1;j--)
+               if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                   matrix[i][j] = 0;
+            if(col == 0 )
+                matrix[i][0] = 0;
         }
+        
     }
 };
