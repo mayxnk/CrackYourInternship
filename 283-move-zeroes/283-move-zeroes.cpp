4ef01af2bc27a38aca057332bb2b442d5fd323f1
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-05-25 18:53:25 +0530

    Time: 44 ms (24.14%), Space: 19.1 MB (83.56%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums)
     {
         //Brute Force Approach - O(2*n)
-        int i = 0;
+        /*int i = 0;
         for(int j = 0;j<nums.size();j++)
         {
             if(nums[j] != 0)
@@ -17,5 +17,20 @@ public:
             nums[i] = 0;
             i++;
         }
+        */
+        //Optimised - O(n),two pointer approach
+        int i = 0,j = 0;
+        while(i<nums.size())
+        {
+            if(nums[i] != 0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else
+                i++;
+        }
+        
     }
 };
