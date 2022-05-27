2a0e7f7ebc4ed6bb39eb87da58efb3f00c96a4cf
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-05-27 23:47:43 +0530

    Time: 31 ms (42.91%), Space: 12.1 MB (11.47%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        vector<int> v;
+        //Brute Force Approach
+        /*vector<int> v;
         for(int i = 0;i<nums.size()-1;i++)
         {
             for(int j = i+1;j<nums.size();j++)
@@ -13,6 +14,25 @@ public:
                     v.push_back(j);
                     break;
                 }
+            }
+        }
+        return v;
+        */
+        //Optimal Approach 
+        int sum = 0,i = 0;
+        vector<int> v;
+        unordered_map<int,int> ourmap;
+        for(int i = 0;i<nums.size();i++)
+            ourmap[nums[i]] = i;
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(ourmap.count(target-nums[i]) && i != ourmap[target - nums[i]])
+            {
+                v.push_back(i);
+                v.push_back(ourmap[target - nums[i]]);
+                ourmap.erase(nums[i]);
+                ourmap.erase(target - nums[i]);
             }
         }
         return v;
