e867e56f4d9ee612c8954bc2b5437caa4969fe7b
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-05-31 21:34:15 +0530

    Time: 121 ms (26.88%), Space: 33.6 MB (79.91%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findDuplicates(vector<int>& nums)
     {
         //Brute foce approach - Time Complexity :O(nlogn),Space - O(1)
-        vector<int> ans;
+        //vector<int> ans;
         /*sort(nums.begin(),nums.end());
         for(int i = 0;i<nums.size()-1;i++)
         {
@@ -15,12 +15,26 @@ public:
         return ans;
         */
         //Better Approach - Time Complexity - O(n),Space -O(n)
+        /*
         unordered_map<int,int> freq;
         for(int i = 0;i<nums.size();i++)
         {
             freq[nums[i]]++;
             if(freq[nums[i]] == 2)
                 ans.push_back(nums[i]);
+        }
+        return ans;
+        */
+        //Optimal Approach - Time Complexity - O(n),Space - O(1)
+        int j = 0;
+        vector<int> ans;
+        for(int i = 0;i<nums.size();i++)
+        {
+            int index = abs(nums[i]) - 1;
+            if(nums[index] > 0)
+                nums[index] = -(nums[index]);
+            else
+                ans.push_back(abs(nums[i]));
         }
         return ans;
     }
