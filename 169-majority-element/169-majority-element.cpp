95c2a7533b7063e727b25bcad28405b11b0fad49
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-07 15:28:52 +0530

    Time: 28 ms (46.75%), Space: 19.5 MB (92.41%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) 
     {
         //Brute Force Approach - Time:O(n),Space : O(n)
-        unordered_map<int,int> freq;
+        /*unordered_map<int,int> freq;
         int ans = -1;
         for(int i = 0;i<nums.size();i++)
         {
@@ -15,5 +15,21 @@ public:
             freq[nums[i]]++;
         }
         return ans;
+        */
+        //Optimal Approach - Time:O(n)
+        int count = 0,major = nums[0];
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(count == 0)
+            {
+                major = nums[i];
+                count++;
+            }
+            else if(major == nums[i])
+                count++;
+            else
+                count--;
+        }
+        return major;
     }
 };
