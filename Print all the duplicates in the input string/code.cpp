8d067cc4089a19d11b0d02f2ebd78cbf22e7d239
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-22 19:59:32 +0530

    Solution code added

--- Print all the duplicates in the input string/code.cpp
+++ Print all the duplicates in the input string/code.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void printDuplicates(string s)
+{
+    //Time - O(2*n),Space - O(n)
+    unordered_map<char,int> mp;
+    for(auto &ch : s)
+    mp[ch]++;
+    for(int i = 0;i<s.size();++i)
+    {
+        if(mp[s[i]] >= 2)
+        {
+            cout<<s[i]<<" count - "<<mp[s[i]]<<endl;
+            mp[s[i]] = 0;
+        }
+    }
+}
+int main()
+{
+    string str = "string check";
+    printDuplicates(str);
+    return 0;
+}
