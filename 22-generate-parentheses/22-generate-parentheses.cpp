7a4fafc7e1c63528a811ac548a4c2c10b6580502
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-27 17:49:18 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (59.03%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -32,26 +32,39 @@ public:
             return true;
         return false;
     }
-    void helper(int n,string str,vector<string>& ans)
+//     void helper(int n,string& str,vector<string>& ans)
+//     {
+//         if(str.size() == (2*n))
+//         {                       
+//             if(checkvalid(str)) 
+//                 ans.push_back(str);       
+//             return;             
+//         }
+// //         helper(n,str + '(',ans);
+        
+// //         helper(n,str + ')',ans);
+//         helper(n,str.append("("),ans);
+//         helper(n,str.append(")"),ans);
+//     }
+    void helper(int n,int start,int close,string str,vector<string>& ans)
     {
         if(str.size() == (2*n))
         {
-            if(checkvalid(str))
-                ans.push_back(str);
-            str.clear();
+            ans.push_back(str);
             return;
         }
-        helper(n,str + '(',ans);
-        
-        helper(n,str + ')',ans);
-        
+        if(start < n)
+            helper(n,start + 1,close,str + '(',ans);
+        if(close < start)
+            helper(n,start,close + 1,str + ')',ans);
     }
     vector<string> generateParenthesis(int n) 
     {
         vector<string> ans;
         string str = "";
-        helper(n,str,ans);
-        
+        //helper(n,str,ans);
+        int start = 0,close = 0;
+        helper(n,start,close,str,ans);
         return ans;
     }
 };
