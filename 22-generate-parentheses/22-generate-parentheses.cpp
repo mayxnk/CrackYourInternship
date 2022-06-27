bd88d0fd612d9a62b0d9a8bc539b1f3beeaddc1c
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-27 12:12:11 +0530

    Time: 85 ms (5.02%), Space: 60.3 MB (5.01%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool checkvalid(string s)
+    {
+        // stack<char> ch;
+        // for(int i = 0;i<s.size();++i)
+        // {
+        //     if(s[i] == '(')
+        //         ch.push(s[i]);
+        //     else if(s[i] == ')' and ch.empty())
+        //         return false;
+        //     else
+        //     {
+        //         ch.pop();
+        //     }
+        // }
+        // if(ch.empty())
+        //     return true;
+        // return false;
+        bool flag = true;
+        int count = 0;
+        for(int i = 0;i<s.size();++i)
+        {
+            if(s[i] == '(')
+                ++count;
+            else
+                --count;
+            if(count < 0)
+                flag = false;
+        }
+        if(flag and count == 0)
+            return true;
+        return false;
+    }
+    void helper(int n,string str,vector<string>& ans)
+    {
+        if(str.size() == (2*n))
+        {
+            if(checkvalid(str))
+                ans.push_back(str);
+            return;
+        }
+        helper(n,str + '(',ans);
+        
+        helper(n,str + ')',ans);
+        
+    }
+    vector<string> generateParenthesis(int n) 
+    {
+        vector<string> ans;
+        string str = "";
+        helper(n,str,ans);
+        
+        return ans;
+    }
+};
